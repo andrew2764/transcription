958ee29ea97d3977f0883d650b6fe3a526ce4719
Andrew <andrew2764@gmail.com>
2024-11-23 21:16:12 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useRef, useState } from "react";
+import "./App.css";
+import ToJyutping from "to-jyutping";
+import pinyin from "pinyin";
+
+function App() {
+  const initialConvertedLyrics = ToJyutping.getJyutpingList("我是一隻小小鳥");
+  const [convertedLyrics, setConvertedLyrics] = useState(
+    initialConvertedLyrics,
+  );
+  const lyricsInputRef = useRef();
+
+  const onConvertJyutping = () => {
+    const jyutpingList = ToJyutping.getJyutpingList(
+      lyricsInputRef.current.value,
+    );
+    setConvertedLyrics(jyutpingList);
+  };
+  const onConvertPinyin = () => {
+    const pinyinListRaw = pinyin(lyricsInputRef.current.value)
+      .flatMap(([py]) => py.replace("\n", ""))
+      .map((py) => (py === "" ? "\n" : py));
+    const pinyinList = pinyinListRaw.map((py, i) => {
+      const char = lyricsInputRef.current.value[i];
+      const pinyin = char in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] ? py : py;
+      return [lyricsInputRef.current.value[i], pinyin];
+    });
+    setConvertedLyrics(pinyinList);
+  };
+
+  return (
+    <div className="container">
+      <textarea
+        ref={lyricsInputRef}
+        id="lyrics-input"
+        rows="10"
+      ></textarea>
+      <section>
+        {convertedLyrics.map(([char, jp], i) => {
+          if (char === "\n") return <br key={i} />;
+
+          return (
+            <span key={i} className="character">
+              <div className="jp">{jp}</div>
+              <div className="char">{char}</div>
+            </span>
+          );
+        })}
+      </section>
+
+      <div className="button-group">
+        <button onClick={onConvertJyutping}>Jyutping</button>
+        <button onClick={onConvertPinyin}>Pinyin</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
